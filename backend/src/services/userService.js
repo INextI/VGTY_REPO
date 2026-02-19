23ebf2936e06e15f0a239d3f5617eb85ad3ee48b
Igor Churikov <igorchrkv@gmail.com>
2026-02-19 20:43:56 +0300

    change services

---
+++ backend/src/services/userService.js
@@ -0,0 +1,89 @@
+const sequelize = require('../config/db');
+const bcrypt = require('bcrypt');
+const User = require('../models/userModel');
+const Student = require('../models/studentModel');
+const Employee = require('../models/employeeModel');
+
+
+//ВЫНЕСТИ ПОТОМ В ОТДЕЛЬНЫЙ МОДУЛЬ
+const saltRounds = 10;
+
+async function hashPassword(plainPassword) {
+    const hash = await bcrypt.hash(plainPassword, saltRounds);
+    return hash;
+}
+//======================
+
+
+
+
+class UserService {
+    async createUser(data) {
+        return await User.create(data)
+    }
+
+    async getAllUsers() {
+        return await User.findAll()
+    }
+
+    async getUserById(id) {
+        return await User.findByPk(id)
+    }
+
+    async updateUser(id, data) {
+        const user = await findByPk(id)
+        if (!user) throw new Error("Пользователь не найден")    // НАПИСАТЬ ПОТОМ ОШИБКУ ОТДЕЛЬНО
+
+        return await user.update(data)
+    }
+
+    async deleteUser(id) {
+        const user = await findByPk(id)
+        if (!user) throw new Error("Пользователь не найден")    // НАПИСАТЬ ПОТОМ ОШИБКУ ОТДЕЛЬНО
+
+        await user.destroy()
+        return { message: 'Удален'}
+    }
+
+    async createFullUser(data) {
+        return await sequelize.transaction(async (t) => {
+            // Создаём пользователя
+            const user = await User.create({
+                login: data.login,
+                password_hash: hashPassword(data.password),
+                is_active: true
+            }, { transaction: t });
+
+            // Если это студент
+            if (data.role === 'student') {
+                await Student.create({
+                    user_id: user.id,
+                    first_name: data.first_name,
+                    last_name: data.last_name,
+                    patronymic: data.patronymic,
+                    birth_date: data.birth_date,
+                    group_id: data.group_id,
+                    education_form_id: data.education_form_id
+                }, { transaction: t });
+            }
+
+            // Если это сотрудник
+            if (data.role === 'employee') {
+                await Employee.create({
+                    user_id: user.id,
+                    first_name: data.first_name,
+                    last_name: data.last_name,
+                    patronymic: data.patronymic,
+                    birth_date: data.birth_date,
+                    role_id: data.role_id,
+                    department_id: data.department_id,
+                    grade_id: data.grade_id
+                }, { transaction: t });
+            }
+
+            return user;
+        });
+    }
+}
+
+module.exports = new UserService()
