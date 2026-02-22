9bf6c6062dc304f4f0443631b5d2f2bc3aa2219a
Igor Churikov <igorchrkv@gmail.com>
2026-02-22 20:30:53 +0300

    change name field

--- backend/src/models/roleModel.js
+++ backend/src/models/roleModel.js
@@ -3,7 +3,7 @@ const {DataTypes} = require('sequelize')
 
 const Role = sequelize.define('role',{
     id: {type: DataTypes.UUID, primaryKey: true, defaultValue: DataTypes.UUIDV4},
-    name: {type: DataTypes.STRING, allowNull: false}
+    name: {type: DataTypes.ENUM('student', 'employee', 'admin'), allowNull: false}
 })
 
 module.exports = Role
