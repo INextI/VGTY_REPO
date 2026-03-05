c8ed81d22f901c3298e3c3136cfac90ba30c51fd
Igor Churikov <igorchrkv@gmail.com>
2026-03-06 02:35:06 +0300

    change func create

--- backend/src/services/eduProgrammService.js
+++ backend/src/services/eduProgrammService.js
@@ -1,8 +1,18 @@
 const eduProgramm = require('../models/eduProgrammModel')
+const facultyService = require('./facultyServices')
+const educationalFormService = require('./educationFormService')
 
 class EduProgrammService {
     async createEduProgramm(data) {
-        return await eduProgramm.create(data)
+        const faculty = await facultyService.getFacultyByName(data.faculty)
+        facultyId = faculty.id
+        const eduForm = await educationalFormService.getEducationFormByName(data.education_form)
+        educationFormId = eduForm.id
+        return await eduProgramm.create({
+            name: data.name,
+            faculty_id: facultyId,
+            education_form_id: educationFormId
+        })
     }
 
     async getAllEduProgramms() {
