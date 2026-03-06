69ea6719a432b572a152644bf4f2c8fb92afb48e
Igor Churikov <igorchrkv@gmail.com>
2026-03-06 22:40:35 +0300

    fix discipineController

--- backend/src/controllers/disciplineController.js
+++ backend/src/controllers/disciplineController.js
@@ -4,7 +4,7 @@ class DisciplineController {
 
     async create(req, res) {
         try {
-            const discipline = await disciplineService.create(
+            const discipline = await disciplineService.createDiscipline(
                 req.body,
                 req.file
             );
@@ -18,7 +18,7 @@ class DisciplineController {
 
     async getAllWithImage(req, res) {
         try {
-            const disciplines = await disciplineService.getAll();
+            const disciplines = await disciplineService.getAllWithImage();
             res.json(disciplines);
         } catch (error) {
             res.status(500).json({ message: error.message });
@@ -27,7 +27,7 @@ class DisciplineController {
 
     async getAllWithoutImage(req, res) {
         try {
-            const disciplines = await disciplineService.getAll();
+            const disciplines = await disciplineService.getAllWithoutImage();
 
             const lightVersion = disciplines.map(d => {
                 const obj = d.toJSON();
@@ -44,7 +44,7 @@ class DisciplineController {
 
     async getByIdWithImage(req, res) {
         try {
-            const discipline = await disciplineService.getById(req.params.id);
+            const discipline = await disciplineService.getByIdWithImage(req.params.id);
             res.json(discipline);
         } catch (error) {
             res.status(404).json({ message: error.message });
@@ -53,7 +53,7 @@ class DisciplineController {
 
     async getByIdWithoutImage(req, res) {
         try {
-            const discipline = await disciplineService.getById(req.params.id);
+            const discipline = await disciplineService.getByIdWithoutImage(req.params.id);
 
             const obj = discipline.toJSON();
             delete obj.image_url;
@@ -67,7 +67,7 @@ class DisciplineController {
 
     async update(req, res) {
         try {
-            const discipline = await disciplineService.update(
+            const discipline = await disciplineService.updateDiscipline(
                 req.params.id,
                 req.body,
                 req.file
@@ -82,7 +82,7 @@ class DisciplineController {
 
     async delete(req, res) {
         try {
-            const result = await disciplineService.delete(req.params.id);
+            const result = await disciplineService.deleteDiscipline(req.params.id);
             res.json(result);
 
         } catch (error) {
@@ -92,93 +92,3 @@ class DisciplineController {
 }
 
 module.exports = new DisciplineController();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-const disciplineService = require('../services/disciplineService')
-
-class DisciplineController {
-    async create(req, res, next) {
-        try{
-            const {name, owner_employee_id, description, image_url, department_id, education_form_id} = req.body
-            const discipline = await disciplineService.createDiscipline(req.body)
-            return res.status(201).json(discipline)
-        } catch (e) {
-            next(e)
-        }
-    }
-
-    async getAll(req,res, next) {
-        try {
-            const discipline = await disciplineService.getAllDisciplines()
-            return res.json(discipline)
-        } catch (e) {
-            next(e)
-        }
-    }
-
-    async getOne(req, res, next) {
-        try {
-            const discipline = await disciplineService.getDisciplineById(req.params.id)
-            return res.json(discipline)
-        } catch (e) {
-            next(e)
-        }
-    }
-
-    async update(req, res, next) {
-        try {
-            const discipline = await disciplineService.updateDiscipline(req.params.id, req.body)
-            return res.json(discipline)
-        } catch (e) {
-            next(e)
-        }
-    }
-
-    async delete(req, res, next) {
-        try {
-            const result = await disciplineService.deleteDiscipline(req.params.id)
-            return res.json(result)
-        } catch (e) {
-            next(e)
-        }
-    }
-}
-
-
-module.exports = new DisciplineController()
-
-*/
