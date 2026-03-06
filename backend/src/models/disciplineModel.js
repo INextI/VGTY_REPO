e8a22b18222b0ab40db1bbf0a2dfa20a343b515f
Igor Churikov <igorchrkv@gmail.com>
2026-03-06 22:41:20 +0300

    fix disciplineModel

--- backend/src/models/disciplineModel.js
+++ backend/src/models/disciplineModel.js
@@ -7,8 +7,8 @@ const Discipline = sequelize.define('discipline',{
     owner_employee_id: {type: DataTypes.UUID},
     description: {type: DataTypes.TEXT},
     image_url: {type: DataTypes.STRING},
-    department_id: {type: DataTypes.UUID},
-    education_form_id: {type: DataTypes.UUID}
+    faculty_id: {type: DataTypes.UUID, allowNull: false},
+    education_form_id: {type: DataTypes.UUID, allowNull: false}
 },{
     tableName: 'disciplines',
     timestamps: true,
