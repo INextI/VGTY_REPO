444f9f249067a8747f89ce9ab96c3d6fa8808b22
Igor Churikov <igorchrkv@gmail.com>
2026-03-06 05:06:27 +0300

    add valid middleware

---
+++ backend/src/middleware/validationMiddleware.js
@@ -0,0 +1,25 @@
+const Joi = require('joi');
+
+const validate = (schema, property = 'body') => {
+    return (req, res, next) => {
+        const {error, value} = schema.validate(req[property], {
+            abortEarly: false,
+            stripUnknown: true
+        });
+
+        if (error) {
+            return res.status(400).json({
+                message: 'Ошибка валидации',
+                erorrs: error.details.map(err => ({
+                    field: err.path.join('.'),
+                    message: err.message
+                }))
+            });
+        }
+
+        req[property] = value;
+        next();
+    }
+}
+
+module.exports = validate;
