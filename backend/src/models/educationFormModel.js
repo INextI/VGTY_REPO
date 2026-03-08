56e4b9ab4f8740852e404dd68b57028c0bcdae30
Igor Churikov <igorchrkv@gmail.com>
2026-03-08 20:32:18 +0300

    fix misspel

--- backend/src/models/educationFormModel.js
+++ backend/src/models/educationFormModel.js
@@ -1,7 +1,7 @@
 const sequelize = require('../config/db')
 const {DataTypes, UUIDV4} = require('sequelize')
 
-const EducationForm = sequelize.define('/education_form',{
+const EducationForm = sequelize.define('education_form',{
     id: {type: DataTypes.UUID, primaryKey: true, defaultValue: UUIDV4},
     name: {type: DataTypes.STRING, allowNull: false, unique: true}
 },{
